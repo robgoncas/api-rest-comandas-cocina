a389063fa45847f8efcca8046d402afdb3d64aef
Roberto Gonzalez <rob.gonzalez.castro@gmail.com>
2026-04-02 21:18:44 -0400

    cambios en package json, node 20, npm install todos los paquetes

--- src/controllers/comanda_controller.js
+++ src/controllers/comanda_controller.js
@@ -6,12 +6,16 @@ export const crear_comanda = async (req, res) => {
 };
 
 export const listar_comandas = async (req, res) => {
+  try {
   const comandas = await Comanda.findAll({
     order: [['fecha_creacion', 'ASC']]
   });
   res.json(comandas);
+} catch (e) {
+  console.error(e);
+  res.status(500).json({ error: 'Error al listar comandas' });  
 };
-
+};
 export const marcar_lista = async (req, res) => {
   const comanda = await Comanda.findByPk(req.params.id);
   comanda.estado = 'lista';
