7709224a04b8fba260d93c2d815b41d96c968ef1
Roberto Gonzalez <rob.gonzalez.castro@gmail.com>
2026-04-02 21:36:47 -0400

    agregando dependencia hstore de pg ... cambiando db js

--- src/config/db.js
+++ src/config/db.js
@@ -17,5 +17,11 @@ dotenv.config();
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-  logging: false
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
