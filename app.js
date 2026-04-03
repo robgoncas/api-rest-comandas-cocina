6617b5575b41984f0201ab45f4bea0caf6c48589
Roberto Gonzalez <rob.gonzalez.castro@gmail.com>
2026-04-02 20:34:15 -0400

    fix: package json , agregando cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv';
 import { sequelize } from './src/config/db.js';
 import auth_routes from './src/routes/auth_routes.js';
 import comanda_routes from './src/routes/comanda_routes.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/auth', auth_routes);
 app.use('/api/comandas', comanda_routes);
