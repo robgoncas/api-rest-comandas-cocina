ab81863cb97ded9d9fb0ac13ab3d267b621a3371
Roberto Gonzalez <rob.gonzalez.castro@gmail.com>
2026-04-06 18:33:12 -0400

    refactor import db into models

--- src/models/Comanda.js
+++ src/models/Comanda.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import { sequelize } from '../config/db.js';
+import sequelize from '../config/db.js';
 
 export const Comanda = sequelize.define('Comanda', {
   texto: DataTypes.STRING,
